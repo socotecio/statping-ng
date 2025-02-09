f7300f6933dd9494ad247a776629d1768e8bf44b
Diane LAKESTANI <diane.lakestani@socotec.com>
2025-02-09 21:28:10 +0100

    fix: no active messages

--- frontend/src/components/Index/MessagesIcon.vue
+++ frontend/src/components/Index/MessagesIcon.vue
@@ -16,6 +16,9 @@ export default {
     },
     methods: {
         activeMessages(msgs) {
+            if (msgs == null) {
+                return 0
+            }
             return msgs.filter(m => this.isAfter(this.now(), m.start_on) && this.isBefore(this.now(), m.end_on)).length
         }
     }
