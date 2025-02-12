695dc9a7a4c1b3be533ac2b8260f58b72851d4ea
Diane LAKESTANI <diane.lakestani@socotec.com>
2025-02-12 10:54:03 +0100

    feat: css

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -23,7 +23,7 @@
                     {{service.name}}
                     <MessagesIcon :messages="service.messages"/>
                   </router-link>
-                  <span class="badge float-right" :class="{'bg-success': service.online, 'bg-danger': !service.online }">{{service.online ? "ONLINE" : "OFFLINE"}}</span>
+                  <span class="badge float-right" :class="{'bg-success': service.online, 'bg-danger': !service.online }">{{service.online ? $t('online') : $t('offline')}}</span>
                   <GroupServiceFailures :service="service"/>
                   <IncidentsBlock :service="service"/>
               </div>
